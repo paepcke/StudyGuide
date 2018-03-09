fa07200d6b1108521f3294a4583e64b5dbd12f3c
hanlin <hanlaung@stanford.edu>
2018-03-09 12:06:47 -0800

    Added tfidf and test queries

--- src/indexing/srtParser.py
+++ src/indexing/srtParser.py
@@ -5,7 +5,6 @@
 #import youtube api
 #porter stemming: for the whole corpus before indexing https://pythonprogramming.net/stemming-nltk-tutorial/ ***
 #stop-wrds ****
-#youtube api ******
 #forum posts for querying data
 #taking queries from random subtititles in srt file as queries ***
 #query expansion: using word2vec to find out most similar words from query to retrieve snippet *****
@@ -18,20 +17,26 @@
 #deal with unseen words
 
 
-#phrase2vec, doc2vec
-#get closest word, if not in word, then go to next closest
-#stopwords: english online
-#tfidf to get the highest ranked words, and also get stopwords through dissimilar videos srt
-#tfidf query (noun phrases,etc)
+#phrase2vec, doc2vec (4)
+#get closest word, if not in word, then go to next closest (5)
+#stopwords: english online (1) -Done
+#tfidf to get the highest ranked words, and also get stopwords through dissimilar videos srt (2) https://stevenloria.com/tf-idf/
+#tfidf query (noun phrases,etc) (3)
 #training
 #ranking of the video snippets indexed by the user through survey
-#tfidf of 1 minute time slots
+#tfidf of 1 minute time slots (6)
+#Porter Stemmer with unique words(7)
 
 #remove words with count of 1
 #find snippet of when this explanation started
 
 import re,sys
 import pysubs2
+import math
+import string
+# -*- coding: utf-8 -*- and from __future__ import division, unicode_literals
+from textblob import TextBlob as tb
+from sklearn.feature_extraction.text import TfidfVectorizer
 from numpy import array
 from PorterStemmer import PorterStemmer
 from nltk.corpus import stopwords
@@ -41,7 +46,61 @@ from gensim import models
 import gensim, logging
 import os
 import random
+from collections import defaultdict
+from sklearn.metrics.pairwise import linear_kernel
+import csv
 
+
+'''
+def buildSrtCorpus(self):
+    lectures = defaultdict(list)
+    with open("data/import/sentences.csv", "r") as sentences_file:
+        reader = csv.reader(sentences_file, delimiter=',')
+        reader.next()
+        for row in reader:
+            episodes[row[1]].append(row[4])
+
+    for episode_id, text in episodes.iteritems():
+        episodes[episode_id] = "".join(text)
+
+corpus = []
+for id, episode in sorted(episodes.iteritems(), key=lambda t: int(t[0])):
+    corpus.append(episode)
+#put in openFile
+'''
+#Had the same issue, should be added through feature_names = tf.get_feature_names() I think. Thanks for the walkthrough, quite fun.
+#feature_names: phrases or n-grams number
+'''
+tfidf_matrix =  tf.fit_transform(corpus)
+feature_names = tf.get_feature_names()
+episode = dense[0].tolist()[0] #filter out to get episode 1 pharses
+phrase_scores = [pair for pair in zip(range(0, len(episode)), episode) if pair[1] > 0]
+'''
+#sorted(phrase_scores, key=lambda t: t[1] * -1)[:5] gives you the sorted score
+'''
+#phrase with scores
+sorted_phrase_scores = sorted(phrase_scores, key=lambda t: t[1] * -1)
+for phrase, score in [(feature_names[word_id], score) for (word_id, score) in sorted_phrase_scores][:20]:
+   print('{0: <20} {1}'.format(phrase, score))
+'''
+
+'''
+#phrases that don't appear in other episodes
+with open("data/import/tfidf_scikit.csv", "w") as file:
+    writer = csv.writer(file, delimiter=",")
+    writer.writerow(["EpisodeId", "Phrase", "Score"])
+
+    doc_id = 0
+    for doc in tfidf_matrix.todense():
+        print "Document %d" %(doc_id)
+        word_id = 0
+        for score in doc.tolist()[0]:
+            if score > 0:
+                word = feature_names[word_id]
+                writer.writerow([doc_id+1, word.encode("utf-8"), score])
+            word_id +=1
+        doc_id +=1
+'''
 class parseSrt:
     def __init__(self):
         self.listFiles = ['closed_caption.srt']
@@ -52,9 +111,60 @@ class parseSrt:
         self.alphanum = re.compile('[^a-zA-Z0-9]')
         self.p = PorterStemmer()
         self.uniqueVocab = set()
-        self.stop_words = set(stopwords.words('english'))
-        self.path = './srtFiles/'
+        self.stop_words = set(self.setupData('stopWords.txt')).union(set(stopwords.words('english')))
+        self.path = './srtFiles/test/'
+        self.documentLen = 4 #sets docLen to 4 lines
+        self.corpus = []
 
+        self.tfidf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0, stop_words = 'english')
+
+
+    def setupData(self, fileName):
+        contents = []
+        f = open(fileName)
+        for line in f:
+            contents.append(line.decode('utf-8'))
+        f.close()
+        result = ('\n'.join(contents)).split()
+        return result
+
+    #Todo: get highest ranked words of each document, do tfidf on srt FORM_FIELD_TO_SALESFORCE_FIELD
+    #Todo: tfidf on the query itself(to limit search on words)
+    #idf gives  the inverse of number of times word appears in other documents, get words with high idf
+    #words with just high tf
+    #phrase queries with tfidf to match highest score....
+
+    def calcTFidfQuery(self, query):
+        response = self.tfidf.transform([query])
+        feature_names = self.tfidf.get_feature_names()
+        print "Query"
+        print response #response- sk learn matrix: (doc_num, feature_name): tfidf score
+        #print feature_names
+        for col in response.nonzero()[1]:
+            print feature_names[col], ', ', response[0, col]
+
+    def calcTFidf(self):
+        tfidf_matrix =  self.tfidf.fit_transform(self.corpus)
+        feature_names = self.tfidf.get_feature_names()
+        dense = tfidf_matrix.todense()
+
+        currDoc = dense[1].tolist()[0] #filter out to get doc (i+1)
+        phrase_scores = [pair for pair in zip(range(0, len(currDoc)), currDoc) if pair[1] > 0] #pair of featurename to feature score
+        #print feature_names
+        sorted_phrase_scores = sorted(phrase_scores, key=lambda t: t[1] * -1)
+        print phrase_scores
+        print "Sorted scores"
+        print sorted_phrase_scores
+        for scoreTuple in sorted_phrase_scores:
+            phraseToScore = str(feature_names[scoreTuple[0]]) + ', ' + str(scoreTuple[1])
+            print phraseToScore
+
+        #for phrase, score in [(feature_names[word_id], score) for (word_id, score) in sorted_phrase_scores][:20]:
+        #    print('{0: <20} {1}'.format(phrase, score))
+        print len(feature_names)
+        print len(currDoc)
+        print phrase_scores
+        self.calcTFidfQuery("This is an MIT srt on computer science")
 
     def getRandomQuery(self):
         word = random.choice(list(self.uniqueVocab))
@@ -93,6 +203,8 @@ class parseSrt:
              #print fileName
         #for fileName in self.listFiles:
              subs = pysubs2.load(fileName)
+             leftDoc = self.documentLen
+             currDoc = ''
              for i in xrange(len(subs)):
                  startTimeStamp = subs[i].start
                  endTimeStamp = subs[i].end
@@ -107,7 +219,17 @@ class parseSrt:
                  line = [xx.strip() for xx in line if xx != '']
                   # stem words
                # line = [self.p.stem(xx) for xx in line if xx not in self.stop_words]
+                 line = [str(xx).translate(None, string.punctuation) for xx in line]
                  line = [xx for xx in line if xx not in self.stop_words]
+
+                 currDoc += " ".join(line) + ' '
+                 leftDoc -= 1
+                 if leftDoc == 0 or i == len(subs) - 1:
+                    print currDoc
+                    self.corpus.append(currDoc)
+                    leftDoc = self.documentLen
+                    currDoc = ''
+
                  for word in line:
                      #print word
                      word = word.lower()
@@ -119,6 +241,7 @@ class parseSrt:
                      self.wordToTime[word] = timeList
                      sentence.append(word)
                  self.text.append(sentence)
+        self.calcTFidf()
 
     def buildPreTrainedVec(self):
         preTrainedModel = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
@@ -147,7 +270,7 @@ def main():
     parser.openFile()
     parser.buildWordVec()
     parser.loadWordVec()
-    parser.getRandomQuery()
+    #parser.getRandomQuery()
 
 if __name__ == "__main__":
     main()
